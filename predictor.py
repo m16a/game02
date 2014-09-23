c950e304504664b3b2f0f11746584e135b5cf318
m16a <mshvorak@gmail.com>
2014-09-23 19:43:18 +0300

    !U minor changes in predictor

--- predictor.py
+++ predictor.py
@@ -1,5 +1,5 @@
 from vector import Vector
-
+from model.Unit import Unit
 
 class Predictor:
 	def __init__(self, top, left, right, bot):
@@ -7,3 +7,18 @@ class Predictor:
 		self.left_side_col = left
 		self.right_side_col = right
 		self.bot_side_col = bot
+
+
+	def predict_unit_move(self, unit, segment):
+		#	p + t * r = q + u * s
+		#	t [0, +inf)
+		#	u [0, 1]  
+
+		p = Vector(unit.x, unit.y)
+		r = Vector(unit.speed_x, unit.speed_y)
+		q = Vector(segment.start.x, segment.start.y)
+		s = Vector(segment.end.x - segment.start.x, segment.end.y - segment.start.y)
+
+		# x - cross point
+        # r - dirrection
+        # symetry reflect r respect to s vector
